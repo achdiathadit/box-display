4086bab337898e29e5fd0e42974674fdb9f53c98
Achdiat Hadit <59441372+achdiathadit@users.noreply.github.com>
2022-08-03 20:42:15 +0700

    Update display

--- src/components/Display4.js
+++ src/components/Display4.js
@@ -1,4 +1,39 @@
+import { useState } from 'react';
+
 const Display4 = () => {
+	const colors = ['red-color', 'green-color', 'blue-color'];
+	const [index, setIndex] = useState(0);
+	const [color1, setColor1] = useState('');
+	const [color2, setColor2] = useState('');
+	const [color3, setColor3] = useState('');
+
+	const handleChangeColor1 = () => {
+		if (index > 2) {
+			setIndex(0);
+			setColor1('');
+		} else {
+			setIndex(index + 1);
+			setColor1(colors[index]);
+		}
+	};
+	const handleChangeColor2 = () => {
+		if (index > 2) {
+			setIndex(0);
+			setColor2('');
+		} else {
+			setIndex(index + 1);
+			setColor2(colors[index]);
+		}
+	};
+	const handleChangeColor3 = () => {
+		if (index > 2) {
+			setIndex(0);
+			setColor3('');
+		} else {
+			setIndex(index + 1);
+			setColor3(colors[index]);
+		}
+	};
 	return (
 		<div className='display-container'>
 			<header>
@@ -8,13 +43,13 @@ const Display4 = () => {
 				</h3>
 			</header>
 			<div className='box-container'>
-				<div className='display-4 box'>
+				<div className={color1 + ' box-4'} onClick={handleChangeColor1}>
 					<p>BOX 1</p>
 				</div>
-				<div className='display-4 box'>
+				<div className={color2 + ' box-4'} onClick={handleChangeColor2}>
 					<p>BOX 2</p>
 				</div>
-				<div className='display-4 box'>
+				<div className={color3 + ' box-4'} onClick={handleChangeColor3}>
 					<p>BOX 3</p>
 				</div>
 			</div>
